1898d7217404364fd7ecccd5f33651ed85e33bac
Pavel Barnz <PavelBarnz91n@gmail.com>
2026-02-22 22:28:25 +0500

    start creating db rolls

---
+++ js/db/db.js
@@ -0,0 +1,586 @@
+export const db = [
+    {
+        name: 'авокадо маки',
+        pic: './images/ttk/avokado-maki.jpg',
+    },
+
+    {
+        name: 'авокадо маки с сыром',
+        pic: './images/ttk/avokado-maki-s-syrom.jpg',
+    },
+
+    {
+        name: 'чикен маки с сыром',
+        pic: './images/ttk/chicken-maki-s-syrom.jpg',
+    },
+
+    {
+        name: 'эби маки',
+        pic: './images/ttk/ebi-maki.jpg',
+    },
+
+    {
+        name: 'эби маки фрай',
+        pic: './images/ttk/ebi-maki-fry.jpg',
+    },
+
+    {
+        name: 'эби маки с сыром',
+        pic: './images/ttk/ebi-maki-s-syrom.jpg',
+    },
+
+    {
+        name: 'кайсу маки',
+        pic: './images/ttk/kaisu-maki.jpg',
+    },
+
+    {
+        name: 'кани маки',
+        pic: './images/ttk/kani-maki.jpg',
+    },
+
+    {
+        name: 'каппа маки',
+        pic: './images/ttk/kappa-maki.jpg',
+    },
+
+    {
+        name: 'магуро маки',
+        pic: './images/ttk/maguro-maki.jpg',
+    },
+
+    {
+        name: 'магуро маки с огурцом спайси',
+        pic: './images/ttk/maguro-maki-s-ogurcom-spaisy.jpg',
+    },
+
+    {
+        name: 'магуро маки с сыром',
+        pic: './images/ttk/maguro-maki-s-syrom.jpg',
+    },
+
+    {
+        name: 'маки сливочный перец',
+        pic: './images/ttk/maki-slivochniy-perec.jpg',
+    },
+
+    {
+        name: 'маки с сыром и икрой массаго',
+        pic: './images/ttk/maki-s-syrom-i-ikroi-masago.jpg',
+    },
+
+    {
+        name: 'манго маки',
+        pic: './images/ttk/mango-maki.jpg',
+    },
+
+    {
+        name: 'саке маки',
+        pic: './images/ttk/sake-maki.jpg',
+    },
+
+    {
+        name: 'саке маки фрай',
+        pic: './images/ttk/sake-maki-fry.jpg',
+    },
+
+    {
+        name: 'саке маки кунсей',
+        pic: './images/ttk/sake-maki-kunsey.jpg',
+    },
+
+    {
+        name: 'саке маки с огурцом',
+        pic: './images/ttk/sake-maki-s-ogurcom.jpg',
+    },
+
+    {
+        name: 'саке маки с сыром',
+        pic: './images/ttk/sake-maki-s-syrom.jpg',
+    },
+
+    {
+        name: 'саке маки спайси',
+        pic: './images/ttk/saki-maki-spaicy.jpg',
+    },
+
+    {
+        name: 'сноу маки',
+        pic: './images/ttk/snow-maki.jpg',
+    },
+
+    {
+        name: 'сноу маки с сыром',
+        pic: './images/ttk/snow-maki-s-syrom.jpg',
+    },
+
+    {
+        name: 'томаго маки',
+        pic: './images/ttk/tomago-maki.jpg',
+    },
+
+    {
+        name: 'томаго маки с сыром',
+        pic: './images/ttk/tomago-maki-s-syrom.jpg',
+    },
+
+    {
+        name: 'унаги маки',
+        pic: './images/ttk/unagi-maki.jpg',
+    },
+
+    {
+        name: 'унаги маки фрай',
+        pic: './images/ttk/unagi-maki-fry.jpg',
+    },
+
+    {
+        name: 'унаги маки с сыром',
+        pic: './images/ttk/unagi-maki-s-syrom.jpg',
+    },
+
+    {
+        name: 'запеченые чикен маки',
+        pic: './images/ttk/zapechenie-chicken-maki.jpg',
+    },
+
+    {
+        name: 'ролл бонито',
+        pic: './images/ttk/roll-bonito.jpg',
+    },
+
+    {
+        name: 'ролл дракон',
+        pic: './images/ttk/roll-drakon.jpg',
+    },
+
+    {
+        name: 'ролл фила с авокадо',
+        pic: './images/ttk/roll-filadephia-avokado.jpg',
+    },
+
+    {
+        name: 'ролл хиросима',
+        pic: './images/ttk/roll-hirosima.jpg',
+    },
+
+    {
+        name: 'ролл калифорния',
+        pic: './images/ttk/roll-kalifornia.jpg',
+    },
+
+    {
+        name: 'ролл калифорния с креветками',
+        pic: './images/ttk/roll-kalifornia-s-krevetkami.jpg',
+    },
+
+    {
+        name: 'ролл калифорния с лососем',
+        pic: './images/ttk/roll-kalifornia-s-lososem.jpg',
+    },
+
+    {
+        name: 'ролл калифорния в кунжуте',
+        pic: './images/ttk/roll-kalifornia-v-kunzhute.jpg',
+    },
+
+    {
+        name: 'ролл канада классик',
+        pic: './images/ttk/roll-kanada-klassic.jpg',
+    },
+
+    {
+        name: 'ролл канада с авокадо',
+        pic: './images/ttk/roll-kanada-s-avokado.jpg',
+    },
+
+    {
+        name: 'ролл лао',
+        pic: './images/ttk/roll-lao.jpg',
+    },
+
+    {
+        name: 'ролл нью йорк',
+        pic: './images/ttk/roll-new-york.jpg',
+    },
+
+    {
+        name: 'ролл фила классик',
+        pic: './images/ttk/roll-philadelphia-classic.jpg',
+    },
+
+    {
+        name: 'ролл фила татаки с креветками',
+        pic: './images/ttk/roll-philadelphia-tataki-s-krevetkami.jpg',
+    },
+
+    {
+        name: 'ролл с лососем в кунжуте',
+        pic: './images/ttk/roll-s-lososem-v-kunzhute.jpg',
+    },
+
+    {
+        name: 'ролл тайга',
+        pic: './images/ttk/roll-taiga.jpg',
+    },
+
+    {
+        name: 'эби сашими',
+        pic: './images/ttk/ebi-sashimi.jpg',
+    },
+
+    {
+        name: 'магуро сашими',
+        pic: './images/ttk/maguro-sashimi.jpg',
+    },
+
+    {
+        name: 'саке сашими',
+        pic: './images/ttk/sashimi-s-lososem.jpg',
+    },
+
+    {
+        name: 'сашими с угрем',
+        pic: './images/ttk/sashimi-s-ugrem.jpg',
+    },
+
+    {
+        name: 'кайсу футо маки',
+        pic: './images/ttk/kaisu-futo-maki.jpg',
+    },
+
+    {
+        name: 'ролл наруто',
+        pic: './images/ttk/roll-naruto.jpg',
+    },
+
+    {
+        name: 'сливочный фреш ролл с тунцом',
+        pic: './images/ttk/slivochnyi-fresh-roll-s-tuncom.jpg',
+    },
+
+    {
+        name: 'канада с лососем',
+        pic: './images/ttk/kanada-s-lososem.jpg',
+    },
+
+    {
+        name: 'ролл сумосан',
+        pic: './images/ttk/roll-sumosan.jpg',
+    },
+
+    {
+        name: 'ясай футо маки',
+        pic: './images/ttk/yasai-futo-maki.jpg',
+    },
+
+    {
+        name: 'запеченый с крабом',
+        pic: './images/ttk/zapecheniy-roll-s-krabom.jpg',
+    },
+
+    {
+        name: 'запеченый с лососем',
+        pic: './images/ttk/zapecheniy-roll-s-lososem.jpg',
+    },
+
+    {
+        name: 'запеченый с тунцом',
+        pic: './images/ttk/zapecheniy-roll-s-tuncom.jpg',
+    },
+
+    {
+        name: 'запеченый с угрем',
+        pic: './images/ttk/zapecheniy-roll-s-ugrom.jpg',
+    },
+
+    {
+        name: 'жареный с креветками и сыром',
+        pic: './images/ttk/zhareniy-roll-s-krevetkami-i-syrom.jpg',
+    },
+
+    {
+        name: 'жареный с лососем и сыром',
+        pic: './images/ttk/zhareniy-roll-s-lososem-i-syrom.jpg',
+    },
+
+    {
+        name: 'жареный с окунем и манго',
+        pic: './images/ttk/zhareniy-roll-s-okunem-i-mango.jpg',
+    },
+
+    {
+        name: 'жареный с угрем и сыром',
+        pic: './images/ttk/zhareniy-roll-s-ugrom-i-syrom.jpg',
+    },
+
+    {
+        name: 'сет аки',
+        pic: './images/ttk/set-aki.jpg',
+    },
+
+    {
+        name: 'сет атака титанов',
+        pic: './images/ttk/set-ataka-titanov.jpg',
+    },
+
+    {
+        name: 'сет классик',
+        pic: './images/ttk/set-classic.jpg',
+    },
+
+    {
+        name: 'сет гунканов',
+        pic: './images/ttk/set-gunkanov.jpg',
+    },
+
+    {
+        name: 'сет хепи дей',
+        pic: './images/ttk/set-happy-day.jpg',
+    },
+
+    {
+        name: 'сет хисока',
+        pic: './images/ttk/set-hisoka.jpg',
+    },
+
+    {
+        name: 'сет хит 2025',
+        pic: './images/ttk/set-hit-2025.jpg',
+    },
+
+    {
+        name: 'сет хот ролл',
+        pic: './images/ttk/set-hot-roll.jpg',
+    },
+
+    {
+        name: 'сет лао',
+        pic: './images/ttk/set-lao.jpg',
+    },
+
+    {
+        name: 'сет наруто',
+        pic: './images/ttk/set-naruto.jpg',
+    },
+
+    {
+        name: 'сет райдо',
+        pic: './images/ttk/set-raido.jpg',
+    },
+
+    {
+        name: 'сет сайтама',
+        pic: './images/ttk/set-saitama.jpg',
+    },
+
+    {
+        name: 'сет теплый',
+        pic: './images/ttk/set-teplyi.jpg',
+    },
+
+    {
+        name: 'сет теплый 2.0',
+        pic: './images/ttk/set-teplyi-2-0.jpg',
+    },
+
+    {
+        name: 'сет теплый мини',
+        pic: './images/ttk/set-teplyi-mini.jpg',
+    },
+
+    {
+        name: 'сет унаги',
+        pic: './images/ttk/set-unagi.jpg',
+    },
+
+    {
+        name: 'сливочный фреш с беконом',
+        pic: './images/ttk/slivochniy-fresh-s-bekonom.jpg',
+    },
+
+    {
+        name: 'магуро маки с огурцом',
+        pic: './images/ttk/moguro-maki-s-ogurcom.jpg',
+    },
+
+    {
+        name: 'эби футо маки фрай',
+        pic: './images/ttk/ebi-futo-maki-frai.jpg',
+    },
+
+    {
+        name: 'эби футо маки спайси',
+        pic: './images/ttk/ebi-futo-maki-spaicy.jpg',
+    },
+
+    {
+        name: 'эби футо маки с сыром',
+        pic: './images/ttk/ebi-futo-maki-s-syrom.jpg',
+    },
+
+    {
+        name: 'кани футо маки с сыром',
+        pic: './images/ttk/kani-futo-maki-s-syrom.jpg',
+    },
+
+    {
+        name: 'ролл хирошима с сыром',
+        pic: './images/ttk/roll-hiroshima-s-syrom.jpg',
+    },
+
+    {
+        name: 'ролл саке фрай в огурце',
+        pic: './images/ttk/roll-sake-frai-v-ogurce.jpg',
+    },
+
+    {
+        name: 'ролл сливочный в икре',
+        pic: './images/ttk/roll-slivochniy-v-ikre.jpg',
+    },
+
+    {
+        name: 'ролл сливочный в кунжуте',
+        pic: './images/ttk/roll-slivochniy-v-kunzhute.jpg',
+    },
+
+    {
+        name: 'ролл сливочный в огурце',
+        pic: './images/ttk/roll-slivochniy-v-ogurce.jpg',
+    },
+
+    {
+        name: 'ролл ура маки',
+        pic: './images/ttk/roll-ura-maki.jpg',
+    },
+
+    {
+        name: 'ролл ура маки кунсей',
+        pic: './images/ttk/roll-ura-maki-kunsei.jpg',
+    },
+
+    {
+        name: 'ролл вегас',
+        pic: './images/ttk/roll-vegas.jpg',
+    },
+
+    {
+        name: 'ролл вегас с сыром',
+        pic: './images/ttk/roll-vegas-s-syrom.jpg',
+    },
+
+    {
+        name: 'саке футо маки фрай',
+        pic: './images/ttk/sake-futo-maki-frai.jpg',
+    },
+
+    {
+        name: 'саке футо маки спайси',
+        pic: './images/ttk/sake-futo-maki-spaicy.jpg',
+    },
+
+    {
+        name: 'саке футо маки с сыром',
+        pic: './images/ttk/sake-futo-maki-s-syrom.jpg',
+    },
+
+    {
+        name: 'саке кунсей с сыром',
+        pic: './images/ttk/sake-kunsei-s-syrom.jpg',
+    },
+
+    {
+        name: 'саке терияки футо маки',
+        pic: './images/ttk/sake-teriaki-futo-maki.jpg',
+    },
+
+    {
+        name: 'тори футо маки',
+        pic: './images/ttk/tori-futo-maki.jpg',
+    },
+
+    {
+        name: 'тори футо маки с сыром',
+        pic: './images/ttk/tori-futo-maki-s-syrom.jpg',
+    },
+
+    {
+        name: 'тори ролл',
+        pic: './images/ttk/tori-roll.jpg',
+    },
+
+    {
+        name: 'тори ролл с сыром',
+        pic: './images/ttk/tori-roll-s-syrom.jpg',
+    },
+
+    {
+        name: 'унаги футо маки фрай',
+        pic: './images/ttk/unagi-futo-maki-frai.jpg',
+    },
+
+    {
+        name: 'тори футо маки фрай',
+        pic: './images/ttk/tori-futo-maki-frai.jpg',
+    },
+
+    {
+        name: 'сет ты и я',
+        pic: './images/ttk/set-ty-i-ya.jpg',
+    },
+
+    {
+        name: 'сет лов из',
+        pic: './images/ttk/set-love-is.jpg',
+    },
+
+    {
+        name: 'рокнрол кимбап с ростбифом',
+        pic: './images/ttk/rock-n-roll-kimbap-s-rostbifom.jpg',
+    },
+
+    {
+        name: 'ролл падшая панда',
+        pic: './images/ttk/roll-padshaya-panda.jpg',
+    },
+
+    {
+        name: 'кей поп кимбап с окунем',
+        pic: './images/ttk/key-pop-kimbap-s-okunem.jpg',
+    },
+
+    {
+        name: 'корейский кимбап со свининой',
+        pic: './images/ttk/koreiskiy-kimbap-so-svininoi.jpg',
+    },
+
+    {
+        name: 'сет ким бап',
+        pic: './images/ttk/set-kimbap.jpg',
+    },
+
+    {
+        name: 'ура ролл с сыром',
+        pic: './images/ttk/ura-roll-s-syrom.jpg',
+    },
+
+    {
+        name: 'сет лойд',
+        pic: './images/ttk/set-lloyd.jpg',
+    },
+
+    {
+        name: 'ролл фила с огурцом',
+        pic: './images/ttk/roll-philadelphia-s-ogurcom.jpg',
+    },
+
+    {
+        name: 'сет авадзи',
+        pic: './images/ttk/set-avadzi.jpg',
+    },
+
+    {
+        name: 'сет окаяма',
+        pic: './images/ttk/set-okayama.jpg',
+    },
+]
